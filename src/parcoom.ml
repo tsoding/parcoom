5dde173d176b8e1a55a7b9f92ca07bb1bf37bf78
rexim <reximkut@gmail.com>
2020-11-20 18:46:02 +0700

    Implement Parcoom.prefix parser

--- src/parcoom.ml
+++ src/parcoom.ml
@@ -1,6 +1,11 @@
 type input =
   { text: string;
-    pos: int
+    pos: int;
+  }
+
+let input_sub (start: int) (len: int) (s: input): input =
+  { text = String.sub (s.text) start len;
+    pos = s.pos + start;
   }
 
 let make_input (s: string): input =
@@ -30,4 +35,24 @@ let bind (f: 'a -> 'b parser) (p: 'a parser): 'b parser =
           match p.run input with
           | Ok (input', x) -> (f x).run input'
           | Error error -> Error error
+  }
+
+let prefix (prefix_str: string): string parser =
+  { run = fun input ->
+          let unexpected_prefix_error =
+            { pos = input.pos;
+              desc = Printf.sprintf "Expected `%s`" prefix_str
+            }
+          in
+          try
+            let prefix_size = String.length prefix_str in
+            let input_size = String.length input.text in
+            let prefix_input = input |> input_sub 0 prefix_size in
+            if String.equal prefix_input.text prefix_str then
+              let rest = input |> input_sub prefix_size (input_size - prefix_size) in
+              Ok (rest, prefix_str)
+            else
+              Error unexpected_prefix_error
+          with
+            Invalid_argument _ -> Error unexpected_prefix_error
   }
